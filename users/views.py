0e2bcc33fedb52d4d6e6b7443e871ce1c49f2a81
Mohammad Albuainain <m.albuainain@tuta.io>
2025-07-28 18:23:23 +0300

    Added register_view and the default UserCreation module

--- users/views.py
+++ users/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
+
+
+def register_view(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("users/login.html")
+    else:
+        form = UserCreationForm()
+        print("hello world")
+    return render(request, "users/register.html", {"form": form})
+
+
+def login_view(request):
+    return render(request, "login.html")
+
