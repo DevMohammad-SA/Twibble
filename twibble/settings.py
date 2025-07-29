63217f12b40be4b048436a78c30f2d2bc1ac9084
Mohammad Albuainain <m.albuainain@tuta.io>
2025-07-29 22:58:52 +0300

    Changed AUTH_USER_MODEL to users.TwibbleUser

--- twibble/settings.py
+++ twibble/settings.py
@@ -30,7 +30,7 @@ DEBUG = os.getenv("DEBUG") == "True"
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split(",")
 
 
-AUTH_USER_MODEL = "users.User"
+AUTH_USER_MODEL = "users.TwibbleUser"
 # Application definition
 
 INSTALLED_APPS = [
