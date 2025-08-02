d5f038256d8b2b57a35727c4fdba6ead8e07810b
Mohammad Albuainain <m.albuainain@tuta.io>
2025-08-02 05:21:10 +0300

    feat(auth): redirect to profile with user ID after login

--- users/views.py
+++ users/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render, redirect
-from .forms import TwibbleUserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404, render, redirect
+from .forms import TwibbleUserCreationForm, TwibbleAuthenticationForm
 from .models import TwibbleUser
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login
+from django.urls import reverse
 
 
 # Create your views here
@@ -16,4 +20,27 @@ def register_view(request):
 
 
 def login_view(request):
-    return render(request, "users/login.html")
+    if request.user.is_authenticated:
+        user = authentication_form.get_user()
+        return redirect("users:profile", user_id=user.id)
+
+    if request.method == "POST":
+        authentication_form = TwibbleAuthenticationForm(
+            request.POST, data=request.POST)
+        if authentication_form.is_valid():
+            user = authentication_form.get_user()
+            login(request, user)
+            return redirect("users:profile", user_id=user.id)
+    else:
+        authentication_form = TwibbleAuthenticationForm()
+
+    return render(
+        request, "users/login.html", {
+            "authentication_form": authentication_form}
+    )
+
+
+@login_required()
+def profile_view(request, user_id):
+    user_obj = get_object_or_404(TwibbleUser, pk=user_id)
+    return render(request, "users/profile.html", {"user": user_obj})
