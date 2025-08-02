d2984cfd94a0cc4adfe1ebf563a216b89225b9e9
Mohammad Albuainain <m.albuainain@tuta.io>
2025-08-02 05:19:09 +0300

    add users/profile/<int:user_id> url pattern

--- users/urls.py
+++ users/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     # Auth
     path("register/", views.register_view, name="register"),
     path("login/", views.login_view, name="login"),
+    path("profile/<int:user_id>", views.profile_view, name="profile"),
 ]
