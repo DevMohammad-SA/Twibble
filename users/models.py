dcfea40eb78c02a73ab687689d1840398c76e009
Mohammad Albuainain <m.albuainain@tuta.io>
2025-08-09 23:18:13 +0300

    fixed default profile image in users/models.py

--- users/models.py
+++ users/models.py
@@ -7,16 +7,12 @@ class TwibbleUser(AbstractUser):
     bio = models.TextField(max_length=300, blank=True)
     is_verified = models.BooleanField(default=False)
     profile_image = models.ImageField(
-        upload_to="profile_images/", null=True, blank=True
+        upload_to="profile_images/",
+        null=True,
+        blank=True,
+        default="profile_images/default.jpg",
     )  # Consider adding a default profile image attribute as follows : default='path/to/defualt/image.jpg'
 
     def __str__(self):
         return self.username
 
-
-class Post(models.Model):
-    text = models.CharField(max_length=300)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    is_reply = models.BooleanField(default=False)
-    user = models.ForeignKey(TwibbleUser, on_delete=models.CASCADE)
