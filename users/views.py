042ea1c9b9dfdb7d43da76027da81585c91a0015
Mohammad Albuainain <m.albuainain@tuta.io>
2025-08-26 17:26:46 +0300

    Add follow/unfollow functionality with views, URLs, and template updates
    - Implemented `follow_view` and `unfollow_view` to handle follow actions
    - Added corresponding URL patterns for follow/unfollow
    - Updated `profile.html` to display a follow/unfollow button
    - Adjusted logic so users cannot follow themselves

--- users/views.py
+++ users/views.py
@@ -48,16 +48,36 @@ def login_view(request):
 def profile_view(request, username):
     profile_user = get_object_or_404(TwibbleUser, username=username)
     tweets = profile_user.tweets.all().order_by("-created_at")
-    current_user = request.user.username
+    current_user = request.user
+    #Check if current_user is already following profile_user
+    already_following = profile_user in current_user.following.all()
+
     context = {
         "profile_user": profile_user,
         "tweets": tweets,
         "current_user": current_user,
+        "already_following":already_following,
     }
     return render(request, "users/profile.html", context)
 
+@login_required
+def follow_view(request,username):
+    profile_user = get_object_or_404(TwibbleUser,username=username)
+    if request.user != profile_user:
+        request.user.following.add(profile_user)
+    else:
+        messages.error(request,"You cannot follow yourself")
+    return redirect("users:profile",username=username)
 
-@login_required()
+
+@login_required
+def unfollow_view(request,username):
+    profile_user = get_object_or_404(TwibbleUser,username=username)
+    if request.user != profile_user:
+        request.user.following.remove(profile_user)
+    return redirect("users:profile",username=username)
+
+@login_required
 def logout_view(request):
     logout(request)
     messages.success(request, "You have been logged out.")
