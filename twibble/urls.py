e60fddfc1ccc8c5b6ed8f18cdbd64c9d43f70b98
Mohammad Albuainain <m.albuainain@tuta.io>
2025-08-28 17:59:04 +0300

    Enable serving media files in development (DEBUG mode)

--- twibble/urls.py
+++ twibble/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from .views import home
 
 urlpatterns = [
@@ -24,3 +26,6 @@ urlpatterns = [
     path("users/", include("users.urls")),
     path("", home, name="home"),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
