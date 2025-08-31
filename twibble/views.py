a13392ae617ca67bbe1317f683aa8cb50b6eba92
Mohammad Albuainain <m.albuainain@tuta.io>
2025-08-31 23:52:30 +0300

    feat: display following users tweets in Home page

--- twibble/views.py
+++ twibble/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import get_object_or_404, render, redirect
-
-
+from users.models import TwibbleUser
+from tweets.models import Tweet
 def home(request):
-    return render(request, "base.html")
+    current_user = request.user
+    following_users = current_user.following.all()
+    tweets = Tweet.objects.filter(user__in=following_users).order_by('-created_at')
+    return render(request, "base.html",{
+        "current_user":current_user,
+        "tweets":tweets,
+        "following_users":following_users
+    })
