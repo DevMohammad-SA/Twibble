74973c295ebe99a6dc60a451a7e16a6325b6cd99
Mohammad Albuainain <m.albuainain@tuta.io>
2025-09-01 21:23:40 +0300

    refactor(users): update profile and follow URLs to use @username format

--- users/urls.py
+++ users/urls.py
@@ -6,10 +6,10 @@ urlpatterns = [
     # Auth
     path("register/", views.register_view, name="register"),
     path("login/", views.login_view, name="login"),
-    path("profile/<str:username>/", views.profile_view, name="profile"),
+    path("@<str:username>/", views.profile_view, name="profile"),
     path("logout/", views.logout_view, name="logout"),
     path("follow/<str:username>/",views.follow_view, name="follow"),
     path("unfollow/<str:username>/",views.unfollow_view,name="unfollow"),
-    path("<str:username>/followers",views.followers_list_view,name="followers_list"),
-    path("<str:username>/followings",views.following_list_view,name="following_list")
+    path("@<str:username>/followers",views.followers_list_view,name="followers_list"),
+    path("@<str:username>/followings",views.following_list_view,name="following_list")
 ]
