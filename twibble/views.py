ef5c643a39a721b465512eb8ab63a0ba5cbc2f25
Mohammad Albuainain <m.albuainain@tuta.io>
2025-09-02 23:41:43 +0300

    fix: handle unauthenticated users on home page

--- twibble/views.py
+++ twibble/views.py
@@ -3,8 +3,12 @@ from users.models import TwibbleUser
 from tweets.models import Tweet
 def home(request):
     current_user = request.user
-    following_users = current_user.following.all()
-    tweets = Tweet.objects.filter(user__in=following_users).order_by('-created_at')
+    if current_user.is_authenticated:
+        following_users = current_user.following.all()
+        tweets = Tweet.objects.filter(user__in=following_users).order_by('-created_at')
+    else:
+        following_users = []
+        tweets = Tweet.objects.all().order_by('-created_at')[:10]
     return render(request, "base.html",{
         "current_user":current_user,
         "tweets":tweets,
