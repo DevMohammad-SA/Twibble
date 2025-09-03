0e6f4ac2413c69dcbccd6331b403e8b4a6f520e3
Mohammad Albuainain <m.albuainain@tuta.io>
2025-09-03 22:42:54 +0300

    enable functional tweet modal with HTMX auto-refresh and modal close

--- tweets/views.py
+++ tweets/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import redirect, render, get_object_or_404
+from django.template.loader import render_to_string
 
+from users.models import TwibbleUser
+from .models import Tweet
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+@login_required()
+def post_view(request):
+    if request.method == "POST":
+        text = request.POST.get("text","").strip()
+
+        if not text:
+            return HttpResponse("<p class='text-danger'>Tweet cannot be empty.</p>")
+
+        tweet = Tweet.objects.create(user=request.user,text=text)
+
+        if request.headers.get("HX-Request"):
+            html = render_to_string("tweets/_tweet_card.html",{"tweet":tweet})
+            return HttpResponse(html)
+        return redirect("home")
+    return redirect("home")
+
