39ee4f40e3451f339c32fe5ab9605074f4b2d7a5
Mohammad Albuainain <m.albuainain@tuta.io>
2026-01-12 12:49:39 +0300

    Add a test case for like functionality

--- tweets/tests.py
+++ tweets/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from django.urls import reverse
+from tweets.models import Tweet
+from users.models import TwibbleUser
 
-# Create your tests here.
+class TestLikeView(TestCase):
+    def setUp(self):
+        self.user = TwibbleUser.objects.create_user(username="user123", password="pass")
+        self.tweet = Tweet.objects.create(user=self.user, text="A test tweet")
+
+    def test_like_tweet(self):
+        self.client.login(username="user123", password="pass")
+        response = self.client.post(reverse("tweets:like", args=[self.tweet.id]))
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(self.tweet.likes.filter(id=self.user.id).exists())
