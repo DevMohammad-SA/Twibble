757416c5ea2953fb57fcde38ef2310b46f8f1a6e
Mohammad Albuainain <m.albuainain@tuta.io>
2026-01-12 14:38:06 +0300

    test(users): add base user creation tests

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
-
+from users.models import TwibbleUser
 # Create your tests here.
+
+
+class UserModelTest(TestCase):
+
+    def test_create_user(self):
+        user = TwibbleUser.objects.create_user(
+                username="testuser",
+                email="test@test.com",
+                password="strongpassword@123"
+                )
+        self.assertEqual(user.username, "testuser")
+        self.assertEqual(user.email, "test@test.com")
+        self.assertTrue(user.check_password("strongpassword@123"))
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
