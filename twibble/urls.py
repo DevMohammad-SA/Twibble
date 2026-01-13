3307e6d18dbba3ac934132261b7d8e483dfb9c30
Mohammad Albuainain <m.albuainain@tuta.io>
2026-01-13 19:03:15 +0300

    implement search functionality for users and tweets with new search.html template

--- twibble/urls.py
+++ twibble/urls.py
@@ -19,13 +19,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import home
+from .views import home, search_view
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("users/", include("users.urls")),
-    path('tweets/',include("tweets.urls")),
+    path("tweets/", include("tweets.urls")),
     path("", home, name="home"),
+    path("search/", search_view, name="search"),
 ]
 
 if settings.DEBUG:
--- twibble/views.py
+++ twibble/views.py
@@ -1,16 +1,46 @@
 from django.shortcuts import get_object_or_404, render, redirect
 from users.models import TwibbleUser
 from tweets.models import Tweet
+from django.db import models
+
+
 def home(request):
     current_user = request.user
     if current_user.is_authenticated:
         following_users = current_user.following.all()
-        tweets = Tweet.objects.filter(user__in=following_users).order_by('-created_at')
+        tweets = Tweet.objects.filter(user__in=following_users).order_by("-created_at")
     else:
         following_users = []
-        tweets = Tweet.objects.all().order_by('-created_at')[:10]
-    return render(request, "base.html",{
-        "current_user":current_user,
-        "tweets":tweets,
-        "following_users":following_users
-    })
+        tweets = Tweet.objects.all().order_by("-created_at")[:10]
+    return render(
+        request,
+        "base.html",
+        {
+            "current_user": current_user,
+            "tweets": tweets,
+            "following_users": following_users,
+        },
+    )
+
+
+def search_view(request):
+    query = request.GET.get("q", "").strip()
+    user_results = []
+    tweet_results = []
+
+    if query:  # only search if query is not empty
+        user_results = TwibbleUser.objects.filter(
+            models.Q(username__icontains=query)
+            | models.Q(first_name__icontains=query)
+            | models.Q(last_name__icontains=query)
+            | models.Q(bio__icontains=query)
+        )
+
+        # search tweets by text            raise
+        tweet_results = Tweet.objects.filter(text__icontains=query)
+    context = {
+        "query": query,
+        "user_results": user_results,
+        "tweet_results": tweet_results,
+    }
+    return render(request, "search_results.html", context)
